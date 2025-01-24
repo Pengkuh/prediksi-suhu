5478acace12f178929b946c9eb2ec3c23dd5d4dc
Pengkuh <sultanpengkuh@gmail.com>
2025-01-24 09:30:31 +0700

    suhu

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template
 import pandas as pd
 import matplotlib
 matplotlib.use('Agg')  # Use non-GUI backend
